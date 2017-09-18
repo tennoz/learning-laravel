0c2e6f7db02b890a65a45dac0c2c2b892df9716b
= <=>
2017-09-18 05:23:53 +0200

    finish controllers

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,6 @@ use App\Task;
 |
 */
 
-Route::get('/tasks', function () {
+Route::get('/tasks', 'TaskController@index');
 
-	// $tasks = DB::table('tasks')->get();
-	$tasks = Task::all();
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id) {
-
-
-	// $task = DB::table('tasks')->find($id);
-
-	$task = Task::find($id);
-    return view('tasks.show', compact('task'));
-});
+Route::get('/tasks/{task}', 'TaskController@show');
