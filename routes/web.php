5623ebf78c5325b4f64dbc1658c7387212356578
= <=>
2017-10-04 21:39:37 +0200

    working with eloquent.

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,17 @@
 <?php
 use App\Task;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function() {
-	$tasks = [
-		'Clean the house.',
-		'Kill the intruders.'
-		'Destroy everything,'
-	]
-	return view('welcome', compact('tasks'));
+Route::get('/tasks', function() {
+
+	$tasks = Task::all();
+
+	return view('tasks.index', compact('tasks'));
 });
 
  
+ Route::get('/tasks/{task}', function($id) {
+	
+	$task = Task::find($id);
+
+	return view('tasks.show', compact('task'));
+});
